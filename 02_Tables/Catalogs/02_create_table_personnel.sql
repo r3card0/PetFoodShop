f67d67c49bb6bc116817ef3daec73be7f553c361
r3card0 <ricardo.rdz@pm.me>
2022-03-02 12:04:51 -0600

    Alter the column id_role_position, indicating is NOT NULL column

--- 02_Tables/Catalogs/02_create_table_personnel.sql
+++ 02_Tables/Catalogs/02_create_table_personnel.sql
@@ -1,7 +1,16 @@
 CREATE TABLE IF NOT EXISTS personnel(id_personnel INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT
-, id_role_position INTEGER UNSIGNED
+, id_role_position INTEGER UNSIGNED NOT NULL
 , personnel_name VARCHAR(100) NOT NULL
 , nick_name VARCHAR(30)
 , phone INTEGER
 , email VARCHAR(100) UNIQUE
-, personnel_description TEXT)
+, personnel_description TEXT);
+-- pensar en agregar --
+-- start_time
+-- last_name1
+-- last_name2
+-- birthdate
+-- active ('Y' / 'N')
+-- para tabla que lleve el historial de asignaciones --
+-- , role_position_start_time esto es otra cosa, es otra tabla que lleve el historial de asignaciones
+--, role position_end_time
